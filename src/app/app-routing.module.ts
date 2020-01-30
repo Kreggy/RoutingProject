5c6f0544660866c7ba227d2c7645eccc7a97a3e2
Kreggy <filip.makrlik@gmail.com>
2020-01-30 22:07:23 +0100

    ok ted to jen udelat pekny

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
